aff060db25e22f3de415fed2373c3d829e47effc
sothouth <55689222+sothouth@users.noreply.github.com>
2024-03-12 00:51:02 +0800

    complate group_table

--- src/bin/server/group_table.rs
+++ src/bin/server/group_table.rs
@@ -1,8 +1,22 @@
-
 use crate::group::Group;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
+pub struct GroupTable(Mutex<HashMap<Arc<String>, Arc<Group>>>);
 
-
-pub struct GroupTable{
-    
+impl GroupTable {
+    pub fn new() -> Self {
+        GroupTable(Mutex::new(HashMap::new()))
+    }
+    pub fn get(&self, key: &String) -> Option<Arc<Group>> {
+        self.0.lock().unwrap().get(key).cloned()
+    }
+    pub fn get_or_create(&self, key: Arc<String>) -> Arc<Group> {
+        self.0
+            .lock()
+            .unwrap()
+            .entry(key.clone())
+            .or_insert_with(|| Arc::new(Group::new()))
+            .clone()
+    }
 }
